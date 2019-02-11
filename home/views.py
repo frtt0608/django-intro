df0731dc0f43d2fa1b00e96dcce53b4fbb6bbc81
frtt0608 <frtt0608@naver.com>
2019-02-11 02:49:04 +0000

    readme

--- home/views.py
+++ home/views.py
@@ -10,11 +10,11 @@ def index(request):
     
 def dinner(request):
     box = ['치킨', '삼겹살', '찌개', '라면', '햄버거']
-    dinner = random.choice(box)
+    dinners = random.choice(box)
     # render 필수인자
     # 1) request, 2) template 파일(html)
     # render 선택인자
     # 3) dictionary : 탬플릿에서 쓸 변수 값을 정의
-    return render(request, 'dinner.html', {'dinner': dinner})
+    return render(request, 'dinner.html', {'dinners': dinners})
     # template는 기본적으로 문법이 jinja2랑 같은데, 장고에서는 DTL을 쓴다.
     # Django Template Language
